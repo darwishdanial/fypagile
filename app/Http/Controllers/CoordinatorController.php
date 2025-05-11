2cbd9d7318e254af6c27fe4799bdc13136d76100
AHMAD DARWISH DANIAL BIN AHMAD NAZRUN <adarwush@gmail.com>
2025-05-11 15:21:05 +0800

    add authorization for every function

--- app/Http/Controllers/CoordinatorController.php
+++ app/Http/Controllers/CoordinatorController.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Http\Controllers;
+
 use Illuminate\Http\Request;
 use App\Services\ProjectLecturerMergerService;
 use App\Services\CoordinatorService;
@@ -10,7 +11,6 @@ use App\Services\SupervisorService;
 use Illuminate\Support\Facades\Auth;
 use Inertia\Inertia;
 
-
 class CoordinatorController extends Controller
 {
     protected $coordinatorService;
@@ -18,19 +18,15 @@ class CoordinatorController extends Controller
     protected $panelService;
     protected $supervisorService;
 
-    public function __construct(CoordinatorService $coordinatorService, StudentService $studentService, PanelService $panelService, SupervisorService $supervisorService){
-
+    public function __construct(CoordinatorService $coordinatorService, StudentService $studentService, PanelService $panelService, SupervisorService $supervisorService)
+    {
         $this->coordinatorService = $coordinatorService;
-
         $this->studentService = $studentService;
-
         $this->panelService = $panelService;
-
         $this->supervisorService = $supervisorService;
     }
 
-    //Home
-
+    // Home
     public function index()
     {
         $this->authorize('view coordinator dashboard');
@@ -41,283 +37,274 @@ class CoordinatorController extends Controller
         $panelsPSM1 = $this->panelService->getPanelPSM1()->count();
         $panelsPSM2 = $this->panelService->getPanelPSM2()->count();
 
-        return Inertia::render('Coordinator/Home/Index',[
+        return Inertia::render('Coordinator/Home/Index', [
             'userName' => $userName,
             'studentsPSM1' => $studentsPSM1,
             'studentsPSM2' => $studentsPSM2,
             'panelsPSM1' => $panelsPSM1,
             'panelsPSM2' => $panelsPSM2
-
         ]);
     }
 
-    //SUPERVISOR ASSIGN
-    //PSM1
-
+    // Supervisor Assign - PSM1
     public function PSM1ListSupervisor()
     {
         $this->authorize('view psm1 assign supervisor table');
 
         $supervisors = $this->supervisorService->getSupervisorPSM1();
 
-        //dd($supervisors);
-
-        return Inertia::render('Coordinator/PSM1/AssignSupervisor',[
+        return Inertia::render('Coordinator/PSM1/AssignSupervisor', [
             'supervisor' => $supervisors
         ]);
     }
 
     public function PSM1SupervisorStudentList($id)
     {
-        $students = $this->studentService->getStudentsSupervisor("PSM1",$id);
+        $this->authorize('view psm1 supervisor student list');
+
+        $students = $this->studentService->getStudentsSupervisor("PSM1", $id);
 
         return $students;
     }
 
     public function PSM1SAssignSupervisor(Request $request)
     {
+        $this->authorize('assign psm1 supervisor');
+
         $studentId = $request->input('studentId');
         $supervisorId = $request->input('supervisorId');
-        
-        $this->studentService->assignStudentsSupervisor("PSM1",$studentId, $supervisorId);
+
+        $this->studentService->assignStudentsSupervisor("PSM1", $studentId, $supervisorId);
     }
 
     public function PSM1UnassignSupervisor($studentId)
     {
-        $this->studentService->unassignStudentsSupervisor("PSM1",$studentId);
+        $this->authorize('unassign psm1 supervisor');
+
+        $this->studentService->unassignStudentsSupervisor("PSM1", $studentId);
     }
 
-
-    //SUPERVISOR ASSIGN
-    //PSM2
-
+    // Supervisor Assign - PSM2
     public function PSM2ListSupervisor()
     {
+        $this->authorize('view psm2 assign supervisor table');
+
         $supervisors = $this->supervisorService->getSupervisorPSM2();
 
-        //dd($supervisors);
-
-        return Inertia::render('Coordinator/PSM2/AssignSupervisorPSM2',[
+        return Inertia::render('Coordinator/PSM2/AssignSupervisorPSM2', [
             'supervisor' => $supervisors
         ]);
     }
 
     public function PSM2SupervisorStudentList($id)
     {
-        $students = $this->studentService->getStudentsSupervisor("PSM2",$id);
+        $this->authorize('view psm2 supervisor student list');
+
+        $students = $this->studentService->getStudentsSupervisor("PSM2", $id);
 
         return $students;
     }
 
     public function PSM2SAssignSupervisor(Request $request)
     {
+        $this->authorize('assign psm2 supervisor');
+
         $studentId = $request->input('studentId');
         $supervisorId = $request->input('supervisorId');
-        
-        $this->studentService->assignStudentsSupervisor("PSM2",$studentId, $supervisorId);
+
+        $this->studentService->assignStudentsSupervisor("PSM2", $studentId, $supervisorId);
     }
 
     public function PSM2UnassignSupervisor($studentId)
     {
-        $this->studentService->unassignStudentsSupervisor("PSM2",$studentId);
+        $this->authorize('unassign psm2 supervisor');
+
+        $this->studentService->unassignStudentsSupervisor("PSM2", $studentId);
     }
 
-    //PANEL ASSIGN
-    //PSM1
-
+    // Panel Assign - PSM1
     public function PSM1listAssignPanel()
     {
         $this->authorize('view psm1 assign panel table');
 
         $panels = $this->panelService->getAssignPanelPSM1();
 
-        return Inertia::render('Coordinator/PSM1/AssignPSM1Panel',[
+        return Inertia::render('Coordinator/PSM1/AssignPSM1Panel', [
             'panel' => $panels
         ]);
     }
 
     public function PSM1PanelStudentList(Request $request)
     {
+        $this->authorize('view psm1 panel student list');
+
         $panelId = $request->input('panelId');
         $type = $request->input('panelTypeValue');
 
-        $students = $this->studentService->getStudentsPanel("PSM1",$panelId, $type);
+        $students = $this->studentService->getStudentsPanel("PSM1", $panelId, $type);
 
         return $students;
     }
 
     public function PSM1SAssignPanel1(Request $request)
     {
+        $this->authorize('assign psm1 panel 1');
+
         $studentId = $request->input('studentId');
         $panelId = $request->input('panelId');
-        
-        $this->studentService->assignStudentsPanel1("PSM1",$studentId, $panelId);
+
+        $this->studentService->assignStudentsPanel1("PSM1", $studentId, $panelId);
     }
 
     public function PSM1SAssignPanel2(Request $request)
     {
+        $this->authorize('assign psm1 panel 2');
+
         $studentId = $request->input('studentId');
         $panelId = $request->input('panelId');
-        
-        $this->studentService->assignStudentsPanel2("PSM1",$studentId, $panelId);
+
+        $this->studentService->assignStudentsPanel2("PSM1", $studentId, $panelId);
     }
 
     public function PSM1UnassignPSMPanel1($studentId)
     {
-        $this->studentService->unassignStudentsPanel1("PSM1",$studentId);
+        $this->authorize('unassign psm1 panel 1');
+
+        $this->studentService->unassignStudentsPanel1("PSM1", $studentId);
     }
 
     public function PSM1UnassignPSMPanel2($studentId)
     {
-        $this->studentService->unassignStudentsPanel2("PSM1",$studentId);
+        $this->authorize('unassign psm1 panel 2');
+
+        $this->studentService->unassignStudentsPanel2("PSM1", $studentId);
     }
 
-    // public function PSM1autoAssignPanelsToStudents(){
-
-    //     $user = Auth::user();
-    //     $email = $user->email;
-    //     $psmType = 'PSM1';
-    //     $this->coordinatorService->autoAssignPanelsToStudents($psmType, $email);
-
-    //     return redirect()->back()->with(['success' => 'AI Panel assignment process has started....']);
-
-    // }
-
-    //PANEL ASSIGN
-    //PSM2
-
+    // Panel Assign - PSM2
     public function PSM2ListPanel()
     {
         $this->authorize('view psm2 assign panel table');
 
         $panels = $this->panelService->getAssignPanelPSM2();
 
-        return Inertia::render('Coordinator/PSM2/AssignPSM2Panel',[
+        return Inertia::render('Coordinator/PSM2/AssignPSM2Panel', [
             'panel' => $panels
         ]);
     }
+
     public function PSM2PanelStudentList(Request $request)
     {
+        $this->authorize('view psm2 panel student list');
+
         $panelId = $request->input('panelId');
         $type = $request->input('panelTypeValue');
 
-        $students = $this->studentService->getStudentsPanel("PSM2",$panelId, $type);
+        $students = $this->studentService->getStudentsPanel("PSM2", $panelId, $type);
 
         return $students;
     }
 
     public function PSM2SAssignPanel1(Request $request)
     {
+        $this->authorize('assign psm2 panel 1');
+
         $studentId = $request->input('studentId');
         $panelId = $request->input('panelId');
-        
-        $this->studentService->assignStudentsPanel1("PSM2",$studentId, $panelId);
+
+        $this->studentService->assignStudentsPanel1("PSM2", $studentId, $panelId);
     }
 
     public function PSM2SAssignPanel2(Request $request)
     {
+        $this->authorize('assign psm2 panel 2');
+
         $studentId = $request->input('studentId');
         $panelId = $request->input('panelId');
-        
-        $this->studentService->assignStudentsPanel2("PSM2",$studentId, $panelId);
+
+        $this->studentService->assignStudentsPanel2("PSM2", $studentId, $panelId);
     }
 
     public function PSM2UnassignPSMPanel1($studentId)
     {
-        $this->studentService->unassignStudentsPanel1("PSM2",$studentId);
+        $this->authorize('unassign psm2 panel 1');
+
+        $this->studentService->unassignStudentsPanel1("PSM2", $studentId);
     }
 
     public function PSM2UnassignPSMPanel2($studentId)
     {
-        $this->studentService->unassignStudentsPanel2("PSM2",$studentId);
+        $this->authorize('unassign psm2 panel 2');
+
+        $this->studentService->unassignStudentsPanel2("PSM2", $studentId);
     }
 
-    //AI
-
-    public function PSM1PredictPanel(){
+    // AI
+    public function PSM1PredictPanel()
+    {
+        $this->authorize('predict psm1 panel');
 
         return $this->coordinatorService->predictAllStudentPanels("PSM1");
-
     }
 
-    public function PSM2PredictPanel(){
-        
-        return $this->coordinatorService->predictAllStudentPanels("PSM2");
+    public function PSM2PredictPanel()
+    {
+        $this->authorize('predict psm2 panel');
 
+        return $this->coordinatorService->predictAllStudentPanels("PSM2");
     }
 
     public function removeAllPanelIdsFromPSM1()
     {
-        return $this->coordinatorService->removeAllPanelIds("PSM1");
+        $this->authorize('remove all psm1 panel ids');
 
+        return $this->coordinatorService->removeAllPanelIds("PSM1");
     }
 
     public function removeAllPanelIdsFromPSM2()
     {
-        return $this->coordinatorService->removeAllPanelIds("PSM2");
+        $this->authorize('remove all psm2 panel ids');
 
+        return $this->coordinatorService->removeAllPanelIds("PSM2");
     }
 
-    public function getMLData(ProjectLecturerMergerService $mergerService){
+    public function getMLData(ProjectLecturerMergerService $mergerService)
+    {
+        $this->authorize('view ml data');
 
         $projectArea = $mergerService->mergePanelAndProjectDataWithMapping(true);
 
-        return Inertia::render('Coordinator/Home/MLData',[
+        return Inertia::render('Coordinator/Home/MLData', [
             'totalPanel' => $projectArea['panelCount'],
-            'totalProjectArea' =>$projectArea['projectAreaCount'],
+            'totalProjectArea' => $projectArea['projectAreaCount'],
             'totalProjectType' => $projectArea['projectTypeCount'],
-            'totalSamples' =>  count($projectArea['samples']),
+            'totalSamples' => count($projectArea['samples']),
             'totalLabels' => count($projectArea['labels']),
             'asgCountPerPanel' => $projectArea['panelAssignments'],
         ]);
-
     }
 
-    public function getPanelHistory(){
+    public function getPanelHistory()
+    {
+        $this->authorize('view panel history');
 
         $users = $this->coordinatorService->getPanelHistory();
 
         return Inertia::render('Coordinator/Home/PanelHistory', [
             'users' => $users
         ]);
-
     }
 
     public function exportAiData()
     {
+        $this->authorize('export ai data');
+
         $this->coordinatorService->exportAiData();
     }
 
-    public function getSampleData(ProjectLecturerMergerService $mergerService){
+    public function getSampleData(ProjectLecturerMergerService $mergerService)
+    {
+        $this->authorize('view sample data');
 
         $mergerService->mergePanelAndProjectDataWithMapping(true);
     }
-
-    // public function PSM1PredictPanel(){
-
-    //     $this->coordinatorService->predictAllStudentPanels("PSM1");
-
-        // $response = Http::timeout(5)->post('http://127.0.0.1:8001/predict-panel', [
-        //     'project_area' => 3,
-        //     'project_type' => 1,
-        // ]);
-        
-        // $predictions = $response->json()['predictions'];
-
-        // $sorted = collect($predictions)->sortDesc();
-
-        // dd($sorted->all());
-
-        // $predictions = 
-
-        // return response()->json($predictions);
-
-        // dd($predictions);
-
-    // }
-
-
-    
-    
-
 }
